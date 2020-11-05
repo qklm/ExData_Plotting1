f22131c47fbe344845309feac5acdd18c7fc2134
David <38164159+qklm@users.noreply.github.com>
2020-11-05 06:28:16 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,14 +1,6 @@
-
-#load in table, and pull necessary dates
-
-hhpow <- read.table("./household_power_consumption.txt", header = TRUE, stringsAsFactors = FALSE)
-subpow <- subset(hhpow,hhpow$Date == "1/2/2007"| hhpow$Date == "2/2/2007")
-
-#'x' must be numeric
-GAP<-as.numeric(subpow$Global_active_power)
-
-#default png() size is h=480 & w=480
-
+data1<-read.table("./household_power_consumption.txt", header = T, stringsAsFactors = F, sep = ";", na.strings = "?")
+data2<-subset(data1, data1$Date >= 1/2/2007 | data1$Date<=2/2/2007)
+gap<-as.numeric(data2$Global_active_power)
 png("plot1.png")
-hist(GAP, col = "Red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+hist(gap, col = "red",main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
 dev.off()
